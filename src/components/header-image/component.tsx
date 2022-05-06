08bc01fb1286ab72574702d37e871320a193ca35
dekitsuneco <47333988+dekitsuneco@users.noreply.github.com>
2022-05-06 05:00:33 +0500

    #5 Create HeaderImage component

--- src/components/header-image/component.tsx
+++ src/components/header-image/component.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { CSSProperties, ReactElement } from 'react';
+import { Stylesheet, variables } from '../../styles';
 
-export default function HeaderImage() {
+const styles: Stylesheet = {
+  imgContainer: {
+    width: '288px',
+    height: '288px',
+  },
+  img: {
+    maxWidth: '100%',
+    borderRadius: variables.border.radius,
+  },
+};
+
+interface HeaderImageStyle {
+  container?: CSSProperties;
+  img?: CSSProperties;
+}
+
+interface HeaderImageProps {
+  source: string;
+  alt: string;
+  style?: HeaderImageStyle;
+}
+
+export default function HeaderImage({
+  source,
+  alt,
+  style: customStyles = {},
+}: HeaderImageProps): ReactElement {
   return (
-    <div>HeaderImage</div>
+    <div 
+      style={{
+        ...styles.imgContainer,
+        ...customStyles?.container,
+      }}
+    >
+    <img 
+      src={source} 
+      alt={alt} 
+      style={{
+        ...styles.img,
+        ...customStyles?.img,
+      }}
+    />
+  </div>
   );
 }
